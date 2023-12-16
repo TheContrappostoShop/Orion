ca7ad72ac67a8b6ebe1b4862ad3a442e1292e54b
Paul_GD <paulskapczyk@gmail.com>
2023-12-16 01:44:32 +0100

    (feat) implement parsing information from SL1 file

--- lib/files/details_screen.dart
+++ lib/files/details_screen.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'dart:io';
-// ignore: depend_on_referenced_packages
+// ignore: unused_import
+import 'dart:math';
 import 'package:flutter/scheduler.dart';
 // ignore: depend_on_referenced_packages
 import 'package:path/path.dart' as path;
@@ -7,6 +9,8 @@ import 'package:flutter/material.dart';
 import 'package:archive/archive.dart';
 import 'package:archive/archive_io.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:ini/ini.dart';
+import 'package:crypto/crypto.dart';
 
 class DetailScreen extends StatefulWidget {
   final File file;
@@ -27,11 +31,124 @@ class _DetailScreenState extends State<DetailScreen> {
   final GlobalKey textKey3 = GlobalKey();
   final GlobalKey textKey4 = GlobalKey();
   final GlobalKey textKey5 = GlobalKey();
+  final GlobalKey textKey6 = GlobalKey();
+
+  String generateHash(String input) {
+    return sha256.convert(utf8.encode(input)).toString();
+  }
+
+  Future<String> extractThumbnail(File sl1File, String subfolder) async {
+    try {
+      final bytes = sl1File.readAsBytesSync();
+      final archive = ZipDecoder().decodeBytes(bytes);
+
+      for (final file in archive) {
+        if (file.name == 'thumbnail/thumbnail400x400.png') {
+          final tempDir = await getTemporaryDirectory();
+          final filePath =
+              '${tempDir.path}/orionDevEnv/$subfolder/${file.name}';
+          final outputFile = File(filePath);
+          outputFile.createSync(recursive: true);
+          outputFile.writeAsBytesSync(file.content as List<int>);
+          return filePath;
+        }
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Error extracting thumbnail.'),
+        ),
+      );
+    }
+
+    return 'assets/images/placeholder.png';
+  }
+
+  Future<String> parseSlicedFile(File sl1File, String key) async {
+    try {
+      final bytes = sl1File.readAsBytesSync();
+      final archive = ZipDecoder().decodeBytes(bytes);
+
+      for (final file in archive) {
+        if (file.name == 'config.ini') {
+          final tempDir = await getTemporaryDirectory();
+          final filePath = '${tempDir.path}/${file.name}';
+          final outputFile = File(filePath);
+          outputFile.createSync(recursive: true);
+          outputFile.writeAsBytesSync(file.content as List<int>);
+
+          // Read the config.ini file as a string
+          final configFileContent = outputFile.readAsStringSync();
+
+          // Parse the config.ini file
+          final config = Config.fromString(configFileContent);
+
+          // Get the value of the key
+          final requested = config.defaults()[key];
+
+          return requested.toString();
+        }
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Error parsing sliced file.'),
+        ),
+      );
+    }
+
+    return 'Error parsing sliced file.';
+  }
+
+  FileStat? fileStat;
+  String fileName = ''; // path.basename(widget.file.path)
+  String layerHeight = ''; // layerHeight
+  String fileSize = ''; // fileStat!.size
+  String modifiedDate = ''; // fileCreationTimestamp
+  String materialName = ''; // materialName
+  String fileExtension = ''; // path.extension(widget.file.path)
+  String thumbnailPath = ''; // extractThumbnail(widget.file, hash)
+  String printTime = ''; // printTime
+  double printTimeInSeconds = 0; // printTime in seconds
+  String materialVolume = ''; // usedMaterial
+  double materialVolumeInMilliliters = 0; // usedMaterial in milliliters
+
+  Future<void> _initFileDetails() async {
+    String hash = generateHash(widget.file.path);
+    layerHeight = await parseSlicedFile(widget.file, 'layerHeight');
+    modifiedDate = await parseSlicedFile(widget.file, 'fileCreationTimestamp');
+    materialName = await parseSlicedFile(widget.file, 'materialName');
+    thumbnailPath = await extractThumbnail(widget.file, hash);
+    printTimeInSeconds =
+        double.parse(await parseSlicedFile(widget.file, 'printTime'));
+    Duration printDuration = Duration(seconds: printTimeInSeconds.toInt());
+    printTime =
+        '${printDuration.inHours.remainder(24).toString().padLeft(2, '0')}:${printDuration.inMinutes.remainder(60).toString().padLeft(2, '0')}:${printDuration.inSeconds.remainder(60).toString().padLeft(2, '0')}';
+    materialVolumeInMilliliters =
+        double.parse(await parseSlicedFile(widget.file, 'usedMaterial'));
+    materialVolume = '${materialVolumeInMilliliters.toStringAsFixed(2)} mL';
+  }
+
+  late ValueNotifier<Future<String>> thumbnailFutureNotifier;
+
+  @override
+  void initState() {
+    super.initState();
+    fileStat = widget.file.statSync();
+    fileName = path.basename(widget.file.path);
+    fileSize = fileStat!.size >= 1000000
+        ? '${fileStat!.size ~/ 1000000} MB'
+        : fileStat!.size >= 1000
+            ? '${fileStat!.size ~/ 1000} KB'
+            : '${fileStat!.size} B';
+    fileExtension = path.extension(widget.file.path);
+    _initFileDetails();
+  }
 
   @override
   Widget build(BuildContext context) {
     SchedulerBinding.instance.addPostFrameCallback((_) {
-      final keys = [textKey1, textKey2, textKey3, textKey4, textKey5];
+      final keys = [textKey1, textKey2, textKey3, textKey4, textKey5, textKey6];
       double maxWidth = 0;
 
       for (var key in keys) {
@@ -50,42 +167,9 @@ class _DetailScreenState extends State<DetailScreen> {
       });
     });
 
-    final FileStat fileStat = widget.file.statSync();
-    final String fileName = path.basename(widget.file.path);
-    final String fileExtension = path.extension(widget.file.path);
-    final String fileSize = '${fileStat.size} bytes';
-    final String creationDate = fileStat.changed.toString().substring(0, 19);
-    final String modifiedDate = fileStat.modified.toString().substring(0, 19);
-
-    Future<String> extractThumbnail(File sl1File) async {
-      try {
-        final bytes = sl1File.readAsBytesSync();
-        final archive = ZipDecoder().decodeBytes(bytes);
-
-        for (final file in archive) {
-          if (file.name == 'thumbnail/thumbnail400x400.png') {
-            final tempDir = await getTemporaryDirectory();
-            final filePath = '${tempDir.path}/${file.name}';
-            final outputFile = File(filePath);
-            outputFile.createSync(recursive: true);
-            outputFile.writeAsBytesSync(file.content as List<int>);
-            return filePath;
-          }
-        }
-      } catch (e) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-            content: Text('Error extracting thumbnail.'),
-          ),
-        );
-      }
-
-      return 'assets/images/placeholder.png';
-    }
-
     return Scaffold(
       appBar: AppBar(
-        title: const Text('File Details'),
+        title: Text('File Details | $modifiedDate'),
       ),
       body: Stack(
         children: [
@@ -109,24 +193,23 @@ class _DetailScreenState extends State<DetailScreen> {
               ),
               const SizedBox(height: 20),
               // ignore: unnecessary_null_comparison
-              if (fileExtension.isNotEmpty)
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding:
-                        EdgeInsets.only(left: leftPadding), // Add left padding
-                    child: FittedBox(
-                      // ignore: unnecessary_null_comparison
-                      child: Text(
-                        'File Extension: $fileExtension',
-                        key: textKey2,
-                        style: const TextStyle(fontSize: 20),
-                      ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding:
+                      EdgeInsets.only(left: leftPadding), // Add left padding
+                  child: FittedBox(
+                    // ignore: unnecessary_null_comparison
+                    child: Text(
+                      'File Size: $fileSize',
+                      key: textKey2,
+                      style: const TextStyle(fontSize: 20),
                     ),
                   ),
                 ),
+              ),
               // ignore: unnecessary_null_comparison
-              if (fileExtension.isNotEmpty) const SizedBox(height: 20),
+              const SizedBox(height: 15),
               Align(
                 alignment: Alignment.centerLeft,
                 child: Padding(
@@ -134,7 +217,7 @@ class _DetailScreenState extends State<DetailScreen> {
                       EdgeInsets.only(left: leftPadding), // Add left padding
                   child: FittedBox(
                     child: Text(
-                      'File Size: $fileSize',
+                      'Layer Height: $layerHeight mm',
                       key: textKey3,
                       style: const TextStyle(fontSize: 20),
                     ),
@@ -149,7 +232,7 @@ class _DetailScreenState extends State<DetailScreen> {
                       EdgeInsets.only(left: leftPadding), // Add left padding
                   child: FittedBox(
                     child: Text(
-                      'Creation Date: $creationDate',
+                      'Material: ${materialName.split('@0.')[0]}',
                       key: textKey4,
                       style: const TextStyle(fontSize: 20),
                     ),
@@ -164,8 +247,22 @@ class _DetailScreenState extends State<DetailScreen> {
                       EdgeInsets.only(left: leftPadding), // Add left padding
                   child: FittedBox(
                     child: Text(
-                      'Modified Date: $modifiedDate',
+                      'Estimated Time: $printTime',
                       key: textKey5,
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: EdgeInsets.only(left: leftPadding),
+                  child: FittedBox(
+                    child: Text(
+                      'Estimated Material: $materialVolume',
+                      key: textKey6,
                       style: const TextStyle(fontSize: 20),
                     ),
                   ),
@@ -181,24 +278,17 @@ class _DetailScreenState extends State<DetailScreen> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  FutureBuilder<String>(
-                    future: extractThumbnail(widget.file),
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        return Image.file(
-                          File(snapshot.data!),
+                  thumbnailPath.isNotEmpty
+                      ? Image.file(
+                          File(thumbnailPath),
                           width: 200,
                           height: 200,
-                        );
-                      } else {
-                        return const Image(
+                        )
+                      : const Image(
                           image: AssetImage('assets/images/placeholder.png'),
                           width: 200,
                           height: 200,
-                        );
-                      }
-                    },
-                  ),
+                        ),
                   const SizedBox(height: kToolbarHeight * 0.4),
                 ],
               ),
