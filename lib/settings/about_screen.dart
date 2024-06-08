d47016f04415d537b12b3c8dae41734ff55ea0f4
Paul_GD <paulskapczyk@gmail.com>
2024-06-08 04:07:56 +0200

    feat(settings): added Developer Mode hidden toggle to AbouScreen
    - tap the QR code on the AboutScreen 5 times to enable Developer Mode

--- lib/settings/about_screen.dart
+++ lib/settings/about_screen.dart
@@ -23,7 +23,10 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:orion/pubspec.dart';
+import 'package:orion/themes/themes.dart';
+import 'package:orion/util/orion_config.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:toastification/toastification.dart';
 
 Future<String> getVersionNumber() async {
   return Pubspec.version;
@@ -36,11 +39,14 @@ class AboutScreen extends StatefulWidget {
   _AboutScreenState createState() => _AboutScreenState();
 }
 
-/// The about screen
 class _AboutScreenState extends State<AboutScreen> {
   double leftPadding = 0;
   double rightPadding = 0;
+  int qrTapCount = 0;
+  bool developerMode = false;
   Color? _standardColor = Colors.white.withOpacity(0.0);
+
+  Toastification toastification = Toastification();
 
   final GlobalKey textKey1 = GlobalKey();
   final GlobalKey textKey2 = GlobalKey();
@@ -70,12 +76,58 @@ class _AboutScreenState extends State<AboutScreen> {
       });
     });
 
+    void handleQrTap() {
+      setState(() {
+        if (OrionConfig().getFlag('developerMode', category: 'advanced')) {
+          toastification.show(
+            context: context,
+            type: ToastificationType.success,
+            style: ToastificationStyle.fillColored,
+            autoCloseDuration: const Duration(seconds: 2),
+            title: const Text('You are already a developer'),
+            alignment: Alignment.topCenter,
+            primaryColor: Colors.green,
+            backgroundColor: Theme.of(context).colorScheme.surface.withBrightness(1.35),
+            foregroundColor: Theme.of(context).colorScheme.onSurface,
+          );
+        } else {
+          qrTapCount++;
+          if (qrTapCount >= 5) {
+            OrionConfig().setFlag('developerMode', true, category: 'advanced');
+            toastification.show(
+              context: context,
+              type: ToastificationType.success,
+              style: ToastificationStyle.fillColored,
+              autoCloseDuration: const Duration(seconds: 2),
+              title: const Text('Developer Mode Activated: You are now a developer!'),
+              alignment: Alignment.topCenter,
+              primaryColor: Colors.green,
+              backgroundColor: Theme.of(context).colorScheme.surface.withBrightness(1.35),
+              foregroundColor: Theme.of(context).colorScheme.onSurface,
+            );
+          } else {
+            toastification.show(
+              context: context,
+              type: ToastificationType.info,
+              style: ToastificationStyle.flatColored,
+              autoCloseDuration: const Duration(seconds: 2),
+              title: Text(
+                  'You are ${5 - qrTapCount} ${5 - qrTapCount == 1 ? 'tap' : 'taps'} away from becoming a developer'),
+              alignment: Alignment.topCenter,
+              primaryColor: Theme.of(context).colorScheme.primary,
+              backgroundColor: Theme.of(context).colorScheme.surface.withBrightness(1.35),
+              foregroundColor: Theme.of(context).colorScheme.onSurface,
+              showProgressBar: false,
+            );
+          }
+        }
+      });
+    }
+
     const String title = kDebugMode ? 'Debug Machine' : 'Prometheus mSLA';
-    const String serialNumber =
-        kDebugMode ? 'S/N: DBG-0001-001' : 'No S/N Available';
+    const String serialNumber = kDebugMode ? 'S/N: DBG-0001-001' : 'No S/N Available';
     const String apiVersion = kDebugMode ? 'Odyssey: Simulated' : 'Odyssey: ?';
-    const String boardType =
-        kDebugMode ? 'Hardware: Debugger' : 'Hardware: Apollo 3.5.2';
+    const String boardType = kDebugMode ? 'Hardware: Debugger' : 'Hardware: Apollo 3.5.2';
 
     return Scaffold(
       body: Stack(
@@ -91,10 +143,7 @@ class _AboutScreenState extends State<AboutScreen> {
                     child: Text(
                       title,
                       key: textKey1,
-                      style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: _standardColor),
+                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: _standardColor),
                     ),
                   ),
                 ),
@@ -121,21 +170,18 @@ class _AboutScreenState extends State<AboutScreen> {
                   child: FittedBox(
                     child: FutureBuilder<String>(
                       future: getVersionNumber(),
-                      builder: (BuildContext context,
-                          AsyncSnapshot<String> snapshot) {
+                      builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
                         if (snapshot.hasData) {
                           return Text(
                             'Orion: ${snapshot.data}',
                             key: textKey3,
-                            style:
-                                TextStyle(fontSize: 20, color: _standardColor),
+                            style: TextStyle(fontSize: 20, color: _standardColor),
                           );
                         } else {
                           return Text(
                             'Orion: N/A',
                             key: textKey3,
-                            style:
-                                TextStyle(fontSize: 20, color: _standardColor),
+                            style: TextStyle(fontSize: 20, color: _standardColor),
                           );
                         }
                       },
@@ -173,23 +219,25 @@ class _AboutScreenState extends State<AboutScreen> {
               ),
             ],
           ),
-          Align(
-            alignment: Alignment.centerRight,
-            child: Padding(
-              padding: EdgeInsets.only(right: rightPadding),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  QrImageView(
-                    data: 'https://github.com/TheContrappostoShop/Orion',
-                    version: QrVersions.auto,
-                    size: 220,
-                    eyeStyle: QrEyeStyle(color: _standardColor),
-                    dataModuleStyle: QrDataModuleStyle(
-                        color: _standardColor,
-                        dataModuleShape: QrDataModuleShape.circle),
-                  ),
-                ],
+          GestureDetector(
+            onTap: handleQrTap,
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: Padding(
+                padding: EdgeInsets.only(right: rightPadding),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    QrImageView(
+                      data: 'https://github.com/TheContrappostoShop/Orion',
+                      version: QrVersions.auto,
+                      size: 220,
+                      eyeStyle: QrEyeStyle(color: _standardColor),
+                      dataModuleStyle:
+                          QrDataModuleStyle(color: _standardColor, dataModuleShape: QrDataModuleShape.circle),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
