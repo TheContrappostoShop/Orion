00b5f29eec48fd3bab973ba5fe969d850a6c6cda
Paul_GD <paulskapczyk@gmail.com>
2024-07-26 15:49:46 +0200

    refactor(util): remove unused import in orion_config.dart

--- lib/util/orion_config.dart
+++ lib/util/orion_config.dart
@@ -17,7 +17,6 @@
 */
 
 import 'dart:io';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:logging/logging.dart';
 import 'package:path/path.dart' as path;
